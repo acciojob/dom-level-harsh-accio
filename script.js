8e94753dbe781f323fea355808ec9571913d7926
harsh-accio <rajputharsh9897@gmail.com>
2025-09-22 09:38:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//your JS code here. If required.
+const child = document.getElementById("level");
+
+let level = 1;
+let current = child;
+ 
+while (current.parentElement && current.parentElement.tagName !== "html") {
+  level++;
+  current = current.parentElement;
+	
+}
+window.alert('The level of the element is: '+level) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
